85d8165d359e9daef1439c10fb208af8227b534a
haoxp <haoxpdp@gmail.com>
2019-10-21 18:19:36 +0800

    add detachment-log

---
+++ detachment-frame/detachment-log/src/main/java/cn/detachment/frame/log/filter/LessThanOrEqualToFilter.java
@@ -0,0 +1,35 @@
+package cn.detachment.frame.log.filter;
+
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.filter.Filter;
+import ch.qos.logback.core.spi.FilterReply;
+
+public class LessThanOrEqualToFilter extends Filter<ILoggingEvent> {
+
+    Level level;
+
+    @Override
+    public FilterReply decide(ILoggingEvent event) {
+        if (!isStarted()) {
+            return FilterReply.NEUTRAL;
+        }
+
+        if (event.getLevel().levelInt <= level.levelInt) {
+            return FilterReply.NEUTRAL;
+        } else {
+            return FilterReply.DENY;
+        }
+    }
+
+    public void setLevel(String level) {
+        this.level = Level.toLevel(level);
+    }
+
+    public void start() {
+        if (this.level != null) {
+            super.start();
+        }
+    }
+}
+
