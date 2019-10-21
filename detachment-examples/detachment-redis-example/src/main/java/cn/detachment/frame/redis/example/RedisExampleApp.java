2414ea1aa87315b8d21d4a67acb49ca015bb06d3
haoxp <haoxpdp@gmail.com>
2019-10-21 22:47:03 +0800

    init detachment-examples, add detachment-redis-example

---
+++ detachment-examples/detachment-redis-example/src/main/java/cn/detachment/frame/redis/example/RedisExampleApp.java
@@ -0,0 +1,16 @@
+package cn.detachment.frame.redis.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author haoxp
+ * @version v1.0
+ * @date 19/10/21 22:28
+ */
+@SpringBootApplication
+public class RedisExampleApp {
+    public static void main(String[] args) {
+        SpringApplication.run(RedisExampleApp.class);
+    }
+}
---
+++ detachment-examples/detachment-redis-example/src/test/java/cn/detachment/frame/redis/example/AppTest.java
@@ -0,0 +1,41 @@
+package cn.detachment.frame.redis.example;
+
+import cn.detachment.frame.redis.example.service.LockTestService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @author haoxp
+ * @version v1.0
+ * @date 19/10/21 22:36
+ */
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class AppTest {
+
+    private static Logger logger = LoggerFactory.getLogger(AppTest.class);
+
+    @Resource
+    private LockTestService lockTestService;
+
+    @Test
+    public void testLock() throws InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        for (int i = 0; i < 10000; i++) {
+            executorService.execute(() -> lockTestService.syncPlusNano());
+        }
+        executorService.shutdown();
+        while (!executorService.isTerminated()) {
+            Thread.sleep(1000);
+        }
+        logger.info("result {}", lockTestService.getNano());
+    }
+}
