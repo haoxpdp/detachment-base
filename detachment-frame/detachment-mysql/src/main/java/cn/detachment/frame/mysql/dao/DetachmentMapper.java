c95d31088776679af9f6d6b0f29fd902ba4b082f
haoxp <haoxpdp@gmail.com>
2019-10-25 23:48:32 +0800

    modify detachmentMapper rm useless method

--- detachment-frame/detachment-mysql/src/main/java/cn/detachment/frame/mysql/dao/DetachmentMapper.java
+++ detachment-frame/detachment-mysql/src/main/java/cn/detachment/frame/mysql/dao/DetachmentMapper.java
@@ -2,7 +2,6 @@ package cn.detachment.frame.mysql.dao;
 
 import cn.detachment.frame.mysql.bean.BaseEntity;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import org.apache.ibatis.annotations.Param;
 
 /**
  * @author haoxp
@@ -10,17 +9,5 @@ import org.apache.ibatis.annotations.Param;
  * @date 19/10/21 23:04
  */
 public interface DetachmentMapper<T extends BaseEntity> extends BaseMapper<T> {
-
-    /**
-     * updateWithVersionByPrimaryKey
-     * 根据id 和 版本号更新
-     *
-     * @param version version
-     * @param record  record
-     * @return int
-     * @author haoxp
-     * @date 19/10/21 23:08
-     */
-    int updateWithVersionByPrimaryKey(@Param("version") Integer version, @Param("record") T record);
 
 }
