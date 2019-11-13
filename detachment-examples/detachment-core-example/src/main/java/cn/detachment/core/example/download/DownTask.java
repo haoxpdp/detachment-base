b78b70561dc8fb9d51a0a449ec8e6f0913ec180e
haoxp <haoxpdp@gmail.com>
2019-11-14 01:04:20 +0800

    update

--- detachment-examples/detachment-core-example/src/main/java/cn/detachment/core/example/download/DownTask.java
+++ detachment-examples/detachment-core-example/src/main/java/cn/detachment/core/example/download/DownTask.java
@@ -1,9 +1,11 @@
 package cn.detachment.core.example.download;
 
-import jdk.internal.util.xml.impl.Input;
 import lombok.Getter;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
