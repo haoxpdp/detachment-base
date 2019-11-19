8aee3f5a863f67f2466ca746b617e720978727a9
haoxp <haoxpdp@gmail.com>
2019-11-20 07:54:41 +0800

    add redisson lock test.

--- detachment-examples/detachment-redis-example/src/test/java/cn/detachment/example/redis/AppTest.java
+++ detachment-examples/detachment-redis-example/src/test/java/cn/detachment/example/redis/AppTest.java
@@ -4,6 +4,8 @@ import cn.detachment.example.redis.service.LockTestService;
 import cn.detachment.example.redis.service.RedisService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,6 +14,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import javax.annotation.Resource;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author haoxp
@@ -48,5 +51,37 @@ public class AppTest {
         redisService.testRedisSet();
         logger.info(redisService.testRedisGetAndRemove());
         logger.info("has key {} ", redisService.testHasKey());
+    }
+
+    private static Integer nano = 0;
+
+    @Resource
+    private RedissonClient redissonClient;
+
+    @Test
+    public void testRedissonLock() throws InterruptedException {
+        String lockN = "testLock";
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        for (int i = 0; i < 10000; i++) {
+            executorService.execute(() -> {
+                RLock rLock = redissonClient.getLock(lockN);
+                try {
+                    boolean res = rLock.tryLock(100,5, TimeUnit.SECONDS);
+                    if (res){
+                        nano ++;
+                        rLock.unlock();
+                    }else {
+                        logger.error("加锁失败！");
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+        executorService.shutdown();
+        while (!executorService.isTerminated()) {
+            Thread.sleep(1000);
+        }
+        logger.info("nano {} ",nano);
     }
 }
