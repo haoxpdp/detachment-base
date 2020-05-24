d2f11674f6cb2b559509cf13ce382206b15e49c9
haoxpzzzz <haoxpdp@gmail.com>
2020-05-25 06:15:08 +0800

    tmp add BackGroundTask

---
+++ core/src/main/java/cn/detachment/core/util/BackGroundTask.java
@@ -0,0 +1,62 @@
+package cn.detachment.core.util;
+
+import java.util.concurrent.*;
+
+/**
+ * @author haoxp
+ * @version v1.0
+ * @date 20/5/25 5:58
+ */
+public abstract class BackGroundTask<V> implements Runnable, Future<V> {
+
+    private final FutureTask<V> computation = new Computation();
+
+    private class Computation extends FutureTask<V> {
+        public Computation() {
+            super(BackGroundTask.this::compute);
+        }
+    }
+
+    @Override
+    public void run() {
+
+    }
+
+    @Override
+    public boolean cancel(boolean mayInterruptIfRunning) {
+        return false;
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return false;
+    }
+
+    @Override
+    public boolean isDone() {
+        return false;
+    }
+
+    @Override
+    public V get() throws InterruptedException, ExecutionException {
+        return null;
+    }
+
+    @Override
+    public V get(long timeout, TimeUnit unit) throws InterruptedException, ExecutionException, TimeoutException {
+        return null;
+    }
+
+    /**
+     * compute
+     * 取消线程
+     *
+     * @return V
+     * @throws Exception 捕获异常
+     * @author haoxp
+     * @date 20/5/25 6:03
+     */
+    protected abstract V compute() throws Exception;
+
+
+}
