f03b68a26ec594d413d2b036a164b363acb019cf
haoxp <haoxpdp@gmail.com>
2020-07-16 17:26:07 +0800

    update SearchConfiguration.java

--- detach-es/src/main/java/cn/detachment/es/adapter/SearchConfiguration.java
+++ detach-es/src/main/java/cn/detachment/es/adapter/SearchConfiguration.java
@@ -10,43 +10,4 @@ import java.util.Map;
  */
 public class SearchConfiguration {
 
-    /**
-     * es search builder includes cache
-     */
-    protected Map<Class<?>, String[]> includesFields = new HashMap<>();
-
-    /**
-     * es search builder exclude cache
-     */
-    protected Map<Class<?>, String[]> excludesFields = new HashMap<>();
-
-
-    public String[] getIncludesFields(Class<?> entityClass) {
-        if (includesFields.containsKey(entityClass)) {
-            return includesFields.get(entityClass);
-        }
-        return null;
-    }
-
-    public String[] getExcludesFields(Class<?> entityClass) {
-        if (excludesFields.containsKey(entityClass)) {
-            return excludesFields.get(entityClass);
-        }
-        return null;
-    }
-
-    public void setIncludesFields(Class<?> clazz, String[] fields) {
-        if (includesFields.containsKey(clazz)) {
-            return;
-        }
-        this.includesFields.put(clazz, fields);
-    }
-
-    public void setExcludesFields(Class<?> clazz, String[] fields) {
-        if (excludesFields.containsKey(clazz)) {
-            return;
-        }
-        this.excludesFields.put(clazz, fields);
-    }
-
 }
