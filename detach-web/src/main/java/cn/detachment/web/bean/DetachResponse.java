6b98dc81154e46c41aee5e5bcf7197c619291536
haoxpzzzz <haoxpdp@gmail.com>
2020-09-18 11:55:14 +0800

    update

--- detach-web/src/main/java/cn/detachment/web/bean/DetachResponse.java
+++ detach-web/src/main/java/cn/detachment/web/bean/DetachResponse.java
@@ -9,7 +9,6 @@ public interface DetachResponse {
      * getCode
      *
      * @return httpCode
-     * @author haoxp
      * @date 20/9/16
      */
     int getCode();
@@ -18,7 +17,6 @@ public interface DetachResponse {
      * getMessage
      *
      * @return error msg describe
-     * @author haoxp
      * @date 20/9/16
      */
     String getMessage();
