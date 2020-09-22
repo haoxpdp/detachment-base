8283f17612f4843fee76c9247b059814c6ede620
haoxpzzzz <haoxpdp@gmail.com>
2020-09-22 23:37:02 +0800

    add testLog

--- detach-example/src/main/java/cn/detachment/example/DetachExample.java
+++ detach-example/src/main/java/cn/detachment/example/DetachExample.java
@@ -2,11 +2,16 @@ package cn.detachment.example;
 
 import cn.detach.api.annoation.RemoteApiScanner;
 import cn.detachment.example.api.TestApi;
+import cn.detachment.example.service.TestService;
+import cn.detachment.web.bean.Result;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 
@@ -16,6 +21,7 @@ import javax.annotation.Resource;
  */
 @SpringBootApplication
 @RemoteApiScanner(scanClasses = TestApi.class)
+@RestController
 public class DetachExample implements CommandLineRunner {
 
     private static final Logger logger = LoggerFactory.getLogger(DetachExample.class);
@@ -30,5 +36,15 @@ public class DetachExample implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         System.out.println(testApi.baidu("https://www.baidu.com"));
+    }
+
+    @Resource
+    private TestService testService;
+
+    @GetMapping("/test")
+    public Result<?> test() {
+        logger.info("test!!!!!!!!!!!!!!!!");
+        testService.test();
+        return Result.success();
     }
 }
---
+++ detach-example/src/main/java/cn/detachment/example/service/TestService.java
@@ -0,0 +1,19 @@
+package cn.detachment.example.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author haoxp
+ * @date 20/9/22
+ */
+@Service
+public class TestService {
+
+    private static final Logger logger = LoggerFactory.getLogger(TestService.class);
+
+    public void test() {
+        logger.info("service test()!!!");
+    }
+}
