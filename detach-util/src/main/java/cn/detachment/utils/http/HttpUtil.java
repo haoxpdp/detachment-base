2280dbd52557b8a72c9eca46cdbccad7670a5351
haoxpzzzz <haoxpdp@gmail.com>
2020-09-30 15:20:54 +0800

    tmp

--- detach-util/src/main/java/cn/detachment/utils/http/HttpUtil.java
+++ detach-util/src/main/java/cn/detachment/utils/http/HttpUtil.java
@@ -1,7 +1,19 @@
 package cn.detachment.utils.http;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpHost;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.protocol.HttpClientContext;
+import org.apache.http.conn.routing.HttpRoute;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.http.protocol.HttpContext;
+
+import java.io.IOException;
 
 /**
  * @author haoxp
@@ -29,5 +41,69 @@ public class HttpUtil {
         return "";
     }
 
+
+    public static void main(String[] args) throws InterruptedException {
+        PoolingHttpClientConnectionManager cm = new PoolingHttpClientConnectionManager();
+        CloseableHttpClient httpClient = HttpClients.custom()
+                .setConnectionManager(cm)
+                .build();
+
+        // URL列表数组
+        String[] urisToGet = {
+                "http://www.domain1.com/",
+                "http://www.domain2.com/",
+                "http://www.domain3.com/",
+                "http://www.domain4.com/"
+        };
+
+        // 为每个url创建一个线程，GetThread是自定义的类
+        GetThread[] threads = new GetThread[urisToGet.length];
+        for (int i = 0; i < threads.length; i++) {
+            HttpGet httpget = new HttpGet(urisToGet[i]);
+            threads[i] = new GetThread(httpClient, httpget);
+        }
+
+        // 启动线程
+        for (int j = 0; j < threads.length; j++) {
+            threads[j].start();
+        }
+
+        // join the threads
+        for (int j = 0; j < threads.length; j++) {
+            threads[j].join();
+        }
+
+    }
+
+    static class GetThread extends Thread {
+
+        private final CloseableHttpClient httpClient;
+        private final HttpContext context;
+        private final HttpGet httpget;
+
+        public GetThread(CloseableHttpClient httpClient, HttpGet httpget) {
+            this.httpClient = httpClient;
+            this.context = HttpClientContext.create();
+            this.httpget = httpget;
+        }
+
+        @Override
+        public void run() {
+            try {
+                CloseableHttpResponse response = httpClient.execute(
+                        httpget, context);
+                try {
+                    HttpEntity entity = response.getEntity();
+                } finally {
+                    response.close();
+                }
+            } catch (ClientProtocolException ex) {
+                // Handle protocol errors
+            } catch (IOException ex) {
+                // Handle I/O errors
+            }
+        }
+
+    }
 
 }
