ef846775d3002a942fdbb4d9edd9d9042e04ebed
haoxp <haoxpdp@gmail.com>
2021-05-19 06:30:15 +0800

    update

--- detach-example/src/main/java/cn/detachment/example/beans/Test.java
+++ detach-example/src/main/java/cn/detachment/example/beans/Test.java
@@ -1,16 +1,12 @@
 package cn.detachment.example.beans;
 
 import cn.detachment.es.utils.ReflectBeanUtil;
-import lombok.Data;
-import sun.reflect.misc.ReflectUtil;
 
 import java.lang.reflect.Method;
 
 /**
  * @author haoxp
- * @date 20/12/15
  */
-//@Data
 public class Test {
 
 
