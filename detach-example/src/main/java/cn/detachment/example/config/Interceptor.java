c2a65057254acc12cf9c84d79d0a33fbf02665c0
haoxp <haoxpdp@gmail.com>
2021-05-23 19:47:17 +0800

    tmp

--- detach-example/src/main/java/cn/detachment/example/config/Interceptor.java
+++ detach-example/src/main/java/cn/detachment/example/config/Interceptor.java
@@ -4,15 +4,12 @@ import cn.detachment.example.annoation.TAop;
 import cn.detachment.utils.http.HttpUtil;
 import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
-import org.springframework.aop.support.DefaultPointcutAdvisor;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.lang.reflect.Method;
 
 /**
  * @author haoxp
- * @date 20/11/4
  */
 @Configuration
 public class Interceptor implements MethodInterceptor {
